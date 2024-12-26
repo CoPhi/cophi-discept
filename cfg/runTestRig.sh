7bd3490bb9c7a3b0f95257b878d2681d678d69ec
Federico Boschetti <federico.boschetti@yahoo.com>
2024-12-26 16:26:21 +0100

    runTestRig.sh modified

--- cfg/runTestRig.sh
+++ cfg/runTestRig.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 GDK_SCALE=2 java -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.ddscale=true -cp antlr-4.7.1-complete.jar:parser.jar org.antlr.v4.gui.TestRig Euporia start annotation.txt -gui
-# oscan4.txt -gui
-#-tokens -tree -trace -diagnostics
+# -gui -tokens -tree -trace -diagnostics
